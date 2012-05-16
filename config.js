88f8a956664aabf677a1bff7ebc081538ecd2486
georgeye <yegh98@yahoo.com>
2012-05-16 15:20:44 -0700

    change log level to info

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 var config = {};
 
 config.logFile = './log/trace.log';
-config.logLevel = 5;
+config.logLevel = 4;
 config.redis_server_port = 6379;
 config.redis_server_name = '127.0.0.1';
 config.default_retry_interval = 10; // in 10 seconds
+config.push_retry_interval = 30; // in 10 seconds
 module.exports = config;
