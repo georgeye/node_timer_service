b136e1fe2357b4e03e94945dd781ef154e8d50c2
georgeye <yegh98@yahoo.com>
2012-05-29 14:53:28 -0700

    update logging

--- utils.js
+++ utils.js
@@ -1,26 +1,34 @@
 var winston = require('winston');
-var config = require('./config');
 
-exports.initLog = function(name) {
+var logLevel = 5;
+exports.initLog = function(name, logLevel_) {
   winston.add(winston.transports.File, { filename: name, maxsize: 1000000});
   winston.remove(winston.transports.Console);
+  logLevel = logLevel_;
 }
 exports.logInfo = function(log) {
-  if(config.logLevel >= 4) {
+  if(logLevel >= 4) {
     var current = new Date();
     winston.info(current + ' PID:' + process.pid + ' ' + log);
   }
 }
 
 exports.logDebug = function(log) {
-  if(config.logLevel >=5) {
+  if(logLevel >=5) {
     var current = new Date();
     winston.debug(current + ' PID:' + process.pid + ' ' + log);
   }
 }
 
+exports.logWarn = function(log) {
+  if(logLevel >= 3) {
+    var current = new Date();
+    winston.warn(current + ' PId:' + process.pid + ' ' + log);
+  }
+}
+
 exports.logError = function(log) {
-  if(config.logLevel >= 2) {
+  if(logLevel >= 2) {
     var current = new Date();
     winston.error(current + ' PID:' + process.pid + ' ' + log);
   }
