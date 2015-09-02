beb3af620a5655dbcfbf88a0efe06c66734bc91f
Nitzan Harel <nitzan.harel@gmail.com>
2015-09-02 11:41:19 -0700

    Really stupid fix but actually makes this work

--- config.js
+++ config.js
@@ -6,4 +6,5 @@ config.redis_server_port = 6379;
 config.redis_server_name = '127.0.0.1';
 config.default_retry_interval = 10; // in 10 seconds
 config.att_convert_retry_interval = 60; // in 60 seconds for transmog
+config.no_retry_services = "value";
 module.exports = config;
