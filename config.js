c5fa0e02867c249e63d4d8448b9fced98e76b355
Martin Hald <mhald@mac.com>
2016-01-27 18:29:50 -0800

    [IK-54] increased retry and made it configurable

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ config.logFile = 'trace.log';
 config.logLevel = 4;
 config.redis_server_port = 6379;
 config.redis_server_name = '127.0.0.1';
+config.max_retry = 5;
 config.default_retry_interval = 10; // in 10 seconds
 config.att_convert_retry_interval = 60; // in 60 seconds for transmog
 config.no_retry_services = ['value'];
